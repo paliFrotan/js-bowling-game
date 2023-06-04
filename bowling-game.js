049091388e0d21972b5be10fb50d429cf46a4ece
Pali Frotan <pali.frotan@icloud.com>
2023-06-04 18:57:51 +0100

    Feat: if one strike implemented

--- bowling-game.js
+++ bowling-game.js
@@ -1,12 +1,21 @@
 export const bowlingGameScore = (playerGameScore) =>
 
-    playerGameScore.replace(/-/g,"0").split(" ").map((val,index,arr) => {
-        // check for 10 
-        let nums = val.split("/").map(Number);
-        let score = nums[0] + nums[1];
-        if (score === 10){
-            score += arr[index+1].split("/").map(Number)[0];
-        }
-        return score;
-    }).reduce((acc,val) => acc+val,0)
+playerGameScore.replace(/-/g,"0").split(" ").map((val,index,arr) => {
 
+    let nums = [];
+    let score = 0;
+    if(Number(val) !== [10]) {
+        nums = val.split("/").map(Number);
+        if (nums[1] === undefined)
+            score = nums[0];
+        else 
+            score = nums[0] + nums[1];
+    }
+    if ((score === 10) && !(nums[1] === undefined)) 
+        score += arr[index+1].replace(/-/g,"0").split("/").map(Number)[0];
+    else if (nums[1] === undefined){
+        score += arr[index+1].replace(/-/g,"0").split("/").map(Number)[0] + arr[index+1].replace(/-/g,"0").split("/").map(Number)[1];
+        
+    };
+    return score;
+}).reduce((acc,val) => acc+val,0);

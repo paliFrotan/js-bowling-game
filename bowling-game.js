717f936d34255367fe88c47e7758a9b84b64036e
Pali Frotan <pali.frotan@icloud.com>
2023-06-04 14:36:09 +0100

    Fix: further reduction

--- bowling-game.js
+++ bowling-game.js
@@ -1,7 +1,6 @@
 export const bowlingGameScore = (playerGameScore) =>
-{
-    
-    let sum = playerGameScore.replace(/-/g,"0").split(" ").map((val,index,arr) => {
+
+    playerGameScore.replace(/-/g,"0").split(" ").map((val,index,arr) => {
         // check for 10 
         let nums = val.split("/").map(Number);
         let score = nums[0] + nums[1];
@@ -10,6 +9,4 @@ export const bowlingGameScore = (playerGameScore) =>
         }
         return score;
     }).reduce((acc,val) => acc+val,0)
-   
-    return sum;
-};
+
